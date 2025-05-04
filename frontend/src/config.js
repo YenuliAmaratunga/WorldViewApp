641256e7c441d0a3e52e8adc9323933d67fa69a6
YenuliAmaratunga <yenulibeemansi@gmail.com>
2025-05-04 18:14:48 +0530

    Switch API URL based on local or deployed version

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,8 @@
-// Use Choreo in production, fallback to localhost in dev
+// Use Choreo in production, fallback to localhost in development 
 export const API_BASE_URL =
   import.meta.env.MODE === "production"
     ? "https://10b8c329-d78f-4b7f-8cd9-448ba1dae2e2-dev.e1-us-east-azure.choreoapis.dev/default/worldview-backend-service/v1.0"
     : "http://localhost:5000";
+
+// export const API_BASE_URL = "https://10b8c329-d78f-4b7f-8cd9-448ba1dae2e2-dev.e1-us-east-azure.choreoapis.dev/default/worldview-backend-service/v1.0";
+// export const API_BASE_URL = "http://localhost:5000";
